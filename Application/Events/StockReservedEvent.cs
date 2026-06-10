746b575aba19c2c6e8913e4577b93c14b0618f06
kma <kma@objctv.ru>
2026-06-10 09:52:32 +0500

    продумал основные ивенты для дальнейшей работы очереди, и интеграции с OrderService

---
+++ Application/Events/StockReservedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Events
+{
+    public class StockReservedEvent
+    {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ReservedAt { get; set; } = DateTime.UtcNow;
+    }
+}
