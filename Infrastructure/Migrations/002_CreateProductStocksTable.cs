1b56f76ab3761fc5b40cd2d81e557581a9f5de34
kma <kma@objctv.ru>
2026-06-11 12:09:42 +0500

    добавил миграции для бд

---
+++ Infrastructure/Migrations/002_CreateProductStocksTable.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+
+namespace Infrastructure.Migrations
+{
+
+    [Migration(20240101002)]
+    public class CreateProductStocksTable : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("product_stocks");
+
+        }
+
+        public override void Up()
+        {
+            Create.Table("product_stocks")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("product_id").AsGuid().NotNullable().ForeignKey("products", "id")
+            .WithColumn("quantity").AsInt32().NotNullable().WithDefaultValue(0)
+            .WithColumn("reserved").AsInt32().NotNullable().WithDefaultValue(0)
+            .WithColumn("warehouse").AsString(100).NotNullable().WithDefaultValue("main")
+            .WithColumn("lead_time_days").AsInt32().NotNullable().WithDefaultValue(3)
+            .WithColumn("created_at").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
+            .WithColumn("updated_at").AsDateTime().Nullable();
+
+            Create.Index("idx_stocks_product").OnTable("product_stocks").OnColumn("product_id").Unique();
+            Create.Index("idx_stocks_quantity").OnTable("product_stocks").OnColumn("quantity");
+        }
+    }
+}
